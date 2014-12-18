4331a169c7b38d140a857f73c79bfe091708048c
Richard Pope <richard@memespring.co.uk>
2014-12-18 13:42:42 +0000

    tidy

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ import os
 class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ['SECRET_KEY']
-    REGISTRY_BASE_URL = os.environ['REGISTRY_BASE_URL']
-    REGISTRY_CONSUMER_KEY = os.environ['REGISTRY_CONSUMER_KEY']
-    REGISTRY_CONSUMER_SECRET = os.environ['REGISTRY_CONSUMER_SECRET']
     BASE_URL = os.environ['BASE_URL']
 
 
--- payment/__init__.py
+++ payment/__init__.py
@@ -1,10 +1,8 @@
 import os
 from flask import Flask
-from flask_oauthlib.client import OAuth
 
 #app
 app = Flask(__name__)
 app.config.from_object(os.environ.get('SETTINGS'))
-oauth = OAuth(app)
 
-from localgov import views
+from payment import views
